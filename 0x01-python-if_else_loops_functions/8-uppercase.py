13f9607d36453ac33043947ad2451ca8a5e24651
ibukun <iadewaleodusanya@gmail.com>
2022-11-29 23:48:19 +0100

    [fixed] pycode formatting

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,7 @@
 def uppercase(str):
     for i in str:
         num = ord(i)
-        #print(num)
         if num >= 97 and num <= 122:
             num = num - 32
-            
         print(f"{num:c}", end="")
     print("")
-        
-
