af0bb79078d598de74a10cce81410411864aaf0c
ibukun <iadewaleodusanya@gmail.com>
2022-12-01 16:24:24 +0100

    [quick fix] condition adjusted

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    print(f"{len(argv) - 1} arguments:")
+    if len(argv) - 1 == 0:
+        print(f"{len(argv) - 1} arguments.")
+    elif len(argv) - 1 == 1:
+        print(f"{len(argv) - 1} argument:")
+    else:
+        print(f"{len(argv) - 1} arguments:")
+    
     for i in range(len(argv)):
         if i != 0:
             print(f"{i}: {argv[i]}")
