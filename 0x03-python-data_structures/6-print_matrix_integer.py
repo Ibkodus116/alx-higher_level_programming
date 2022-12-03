52ddc7a8ca869245a7583d988c2fa2ad07cb13ec
ibukun <iadewaleodusanya@gmail.com>
2022-12-03 13:45:34 +0100

    intiger fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         lent = len(i) - 1
         for j in i:
             if j != i[lent]:
-                print("{}".format(j), end=" ")
+                print("{:d}".format(j), end=" ")
             if j == i[lent]:
-                print("{}".format(j), end="")
+                print("{:d}".format(j), end="")
         print()
