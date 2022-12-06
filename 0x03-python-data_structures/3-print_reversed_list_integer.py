c6045fcf5057bb7cb09f70af89d70889257d611e
ibukun <iadewaleodusanya@gmail.com>
2022-12-06 12:03:22 +0100

    [correction] fixed errors as corrections

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
+        #print("{:d}".format(9))
         pass
-    new_list = my_list.copy()
-    new_list.reverse()
-    for i in new_list:
-        print("{:d}".format(i))
+    else:
+        new_list = my_list.copy()
+        new_list.reverse()
+        for i in new_list:
+            print("{:d}".format(i))
