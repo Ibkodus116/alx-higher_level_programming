e100c732879f558f62eb5c9ee5ab5d7d7817d913
ibukun <iadewaleodusanya@gmail.com>
2022-12-08 13:42:13 +0100

    [fixed] case of 0 fixed

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    mul = 0
-    add = 0
-    for i in range(len(my_list)):
-        mul += my_list[i][0] * my_list[i][1]
-        add += my_list[i][1]
-    return mul / add
+    if len(my_list) == 0:
+        return 0
+    else:
+        mul = 0
+        add = 0
+        for i in range(len(my_list)):
+            mul += my_list[i][0] * my_list[i][1]
+            add += my_list[i][1]
+        return mul / add
