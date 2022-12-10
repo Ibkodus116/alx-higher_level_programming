793b63bdac0abe1a1db27f5d4dbb3679b22b95b7
ibukun <iadewaleodusanya@gmail.com>
2022-12-10 05:56:51 +0100

    [completed] unbelivable

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    if roman_string is None:
+        return 0
+
     rom = {
         "I": 1,
         "V": 5,
@@ -8,26 +13,26 @@ def roman_to_int(roman_string):
         "C": 100,
         "D": 500,
         "M": 1000,
-        "CD": 400,
-        "DC": 600,
-        "DCCC": 800,
-        "CM": 900,
-        "XL": 40,
-        "LX": 60,
-        "LXX": 70,
-        "LXXX": 80,
-        "XC": 90,
         "IV": 4,
-        "VI": 6,
-        "VII": 7,
-        "VIII": 8,
-        "IX": 9
+        "IX": 9,
+        "XL": 40,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900,
         }
     i = 0
     num = 0
     rom_list = list(rom)
-    roman = roman_string
-    for i in roman_string:
-        if i in rom_list:
-            num += rom[i]
+
+#    for i in roman_string:
+#        if i in rom_list:
+#            num += rom[i]
+    while i < len(roman_string):
+        two = roman_string[i:i+2]
+        if two in rom_list:
+            num += rom[two]
+            i += 1
+        else:
+            num += rom[roman_string[i]]
+        i += 1
     return num
