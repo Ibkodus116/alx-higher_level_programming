fa262e95c6948ca54ccfc26018787577276e5b86
ibukun <iadewaleodusanya@gmail.com>
2022-12-20 05:05:23 +0100

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[num]}", end="")
             num += 1
     except IndexError:
-        print("\n")
+        print()
         return num
 
-    print("\n")
+    print()
     return num
