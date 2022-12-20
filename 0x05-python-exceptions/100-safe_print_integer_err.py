dfbc63d42a3a41872c9b421fbe1f903e210c278d
ibukun <iadewaleodusanya@gmail.com>
2022-12-20 07:19:06 +0100

    [extra]

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as i:
+        stderr.write("Exception: {}\n".format(i))
+        return False
+    else:
+        return True
