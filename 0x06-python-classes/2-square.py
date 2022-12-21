d81f5e23ffff52dcb8a10dc09e6a542da5c06226
ibukun <iadewaleodusanya@gmail.com>
2022-12-21 18:01:34 +0100

    [completed] task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class Square that defines a square by: (based on 1-square.py).
+"""
+
+
+class Square:
+    """Class Square that defines a square by: (based on 1-square.py).
+
+    the __init__ is to initialise our Square class
+
+    args:
+        size (int): the size of the square
+    """
+    def __init__(self, size=0):
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
