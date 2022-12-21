e7fc3856e6e2eca196a4ccb633289b3acdf0aa22
ibukun <iadewaleodusanya@gmail.com>
2022-12-21 15:42:34 +0100

    [figuring] things out

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """Creating an empty class called Square.
+    """Class Square defines a square.
 
     The class does absolutly nothng and it's
     known as the simpliest class to ever be created.
