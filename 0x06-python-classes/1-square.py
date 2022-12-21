c28c11e90b7922ab4fd5629580488e0d18a6cb25
ibukun <iadewaleodusanya@gmail.com>
2022-12-21 16:11:43 +0100

    [completed] task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Square that defines a square by: (based on 0-square.py).
+"""
+
+
+class Square:
+    """Class Square that defines a square by: (based on 0-square.py).
+
+    the __init__ is to initialise our Square class
+
+    args:
+        size (int): the size of the square
+    """
+    def __init__(self, size) -> None:
+        self.__size = size
