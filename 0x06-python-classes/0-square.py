c5d2f146f6a60a65c2a19e5ad38a1a6cebb48eb9
ibukun <iadewaleodusanya@gmail.com>
2022-12-21 15:37:13 +0100

    initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Creating an empty class called Square.
+
+    The class does absolutly nothng and it's
+    known as the simpliest class to ever be created.
+    """
+    pass
