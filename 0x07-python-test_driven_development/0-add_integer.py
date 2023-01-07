27819495bbe5fd226595d29e6c6233d0236ad38d
ibukun <iadewaleodusanya@gmail.com>
2023-01-07 18:26:21 +0100

    [Task 0] comlpted

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,32 @@
+#!/usr/bin/python3
+"""
+
+Module containing a function that add two number
+
+"""
+
+
 def add_integer(a, b=98):
+    """Function adding two numbers
+    Args:
+        a: First Number
+        b: second Number
+
+    Return:
+        Returns the sum of arg1 and arg2
+
+    Raises:
+        TypeError: stating that a or b must be and intiger or float
+
+    """
+
     if isinstance(a, int) or isinstance(a, float):
-        c = int(a)
+        a = int(a)
     else:
         raise TypeError("a must be an integer")
     if isinstance(b, int) or isinstance(b, float):
-        d = int(b)
+        b = int(b)
     else:
         raise TypeError("b must be an integer")
 
-    return c + d
+    return a + b
