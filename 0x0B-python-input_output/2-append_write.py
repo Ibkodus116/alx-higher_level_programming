06079d0d8bbe3f6ceedf764a031143da41d3d038
ibukun <iadewaleodusanya@gmail.com>
2023-01-17 06:57:07 +0100

    fixed

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,9 @@
 Module for appending a file
 """
 
+
 def append_write(filename="", text=""):
+    """Function Appending to a text"""
+
     with open(filename, 'a') as f:
         return f.write(text)
