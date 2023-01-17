bd0eb077189a6707d84819403bdfbd5ce5be8030
ibukun <iadewaleodusanya@gmail.com>
2023-01-17 06:50:13 +0100

    fixed

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module handling write function
+
+"""
+
+
+def write_file(filename="", text=""):
+    """Funcion writting a file"""
+    with open(filename, "w") as f:
+        return f.write(text)
