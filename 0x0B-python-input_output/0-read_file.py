62449042f30539a718830af209df250060865870
ibukun <iadewaleodusanya@gmail.com>
2023-01-17 06:35:21 +0100

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+Module for file opening
+"""
 def read_file(filename="", encoding="utf-8"):
+    """Function Opening a file for reading"""
     with open(filename) as f:
         print(f.read())
