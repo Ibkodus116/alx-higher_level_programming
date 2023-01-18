95030267531f1be2d7eccfd1abb9f761cd43fb37
ibukun <iadewaleodusanya@gmail.com>
2023-01-18 06:23:34 +0100

    completed

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module Doc for a longe thing"""
+
+
+class Base:
+    """Class Base Doc"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
