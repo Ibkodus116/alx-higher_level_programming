c4c6debfeca5f267bdc9ca40cfb1567d4fb7119c
ibukun <iadewaleodusanya@gmail.com>
2023-01-18 22:37:16 +0100

    another attempt

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,48 +8,48 @@ class Rectangle(Base):
     """The rectangle Class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """The initializer"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super(Rectangle, self).__init__(id)
 
     @property
     def width(self):
         """Width Getter"""
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """Setterf for width"""
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
         """height Getter"""
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """Setterf for height"""
-        self.height = value
+        self.__height = value
 
     @property
     def x(self):
         """Variable x Getter"""
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """Setterf for x"""
-        self.x = value
+        self.__x = value
 
     @property
     def y(self):
         """Variable y Getter"""
-        return self.y
+        return self.__y
 
     @y.setter
     def y(self, value):
         """Setterf for y"""
-        self.y = value
+        self.__y = value
