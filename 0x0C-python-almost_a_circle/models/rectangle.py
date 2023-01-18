45099e0c6962f5aeb3b6a64f4775e151d4a419a8
ibukun <iadewaleodusanya@gmail.com>
2023-01-18 22:31:01 +0100

    attempted

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,54 @@
 #!/usr/bin/python3
+from models.base import Base
 
-""""""
+"""Module handling the rectangle function"""
+
+
+class Rectangle(Base):
+    """The rectangle Class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super(Rectangle, self).__init__(id)
+
+    @property
+    def width(self):
+        """Width Getter"""
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """Setterf for width"""
+        self.width = value
+
+    @property
+    def height(self):
+        """height Getter"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """Setterf for height"""
+        self.height = value
+
+    @property
+    def x(self):
+        """Variable x Getter"""
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        """Setterf for x"""
+        self.x = value
+
+    @property
+    def y(self):
+        """Variable y Getter"""
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        """Setterf for y"""
+        self.y = value
