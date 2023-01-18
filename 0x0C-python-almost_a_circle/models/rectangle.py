0af884d83e85261005ef92c4388fe9a48323f20b
ibukun <iadewaleodusanya@gmail.com>
2023-01-18 22:33:13 +0100

    doc updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from models.base import Base
-
 """Module handling the rectangle function"""
+
+from models.base import Base
 
 
 class Rectangle(Base):
     """The rectangle Class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """The initializer"""
         self.__width = width
         self.__height = height
         self.__x = x
