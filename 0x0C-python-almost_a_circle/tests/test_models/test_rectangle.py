805900a93fe4668f15437e3a5ca9262823f990f5
ibukun <iadewaleodusanya@gmail.com>
2023-01-19 12:09:36 +0100

    test cases

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,74 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def test_init(self):
+        r = Rectangle(10, 20, 30, 40)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 20)
+        self.assertEqual(r.x, 30)
+        self.assertEqual(r.y, 40)
+        self.assertIsInstance(r, Rectangle)
+        self.assertIsInstance(r, Base)
+
+    def test_init_with_no_arg(self):
+        r = Rectangle(10, 20)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+
+    def test_init_with_id(self):
+        r = Rectangle(10, 20, id=1)
+        self.assertEqual(r.id, 1)
+
+    def test_width_setter(self):
+        r = Rectangle(10, 20)
+        r.width = 30
+        self.assertEqual(r.width, 30)
+
+    def test_width_setter_with_invalid_value(self):
+        r = Rectangle(10, 20)
+        with self.assertRaises(TypeError):
+            r.width = "30"
+        with self.assertRaises(ValueError):
+            r.width = -30
+
+    def test_height_setter(self):
+        r = Rectangle(10, 20)
+        r.height = 30
+        self.assertEqual(r.height, 30)
+
+    def test_height_setter_with_invalid_value(self):
+        r = Rectangle(10, 20)
+        with self.assertRaises(TypeError):
+            r.height = "30"
+        with self.assertRaises(ValueError):
+            r.height = -30
+
+    def test_x_setter(self):
+        r = Rectangle(10, 20)
+        r.x = 30
+        self.assertEqual(r.x, 30)
+
+    def test_x_setter_with_invalid_value(self):
+        r = Rectangle(10, 20)
+        with self.assertRaises(TypeError):
+            r.x = "30"
+        with self.assertRaises(ValueError):
+            r.x = -30
+
+    def test_y_setter(self):
+        r = Rectangle(10, 20)
+        r.y = 30
+        self.assertEqual(r.y, 30)
+
+    def test_y_setter_with_invalid_value(self):
+        r = Rectangle(10, 20)
+        with self.assertRaises(TypeError):
+            r.y = "30"
+        with self.assertRaises(ValueError):
+            r.y = -30
+
+    def test_area(self):
+        r = Rectangle(10, 20)
+        self.assertEqual(r.area(), 200)
