6553d46858ff17f159d533d188c1fab03d157ab0
ibukun <iadewaleodusanya@gmail.com>
2023-02-19 08:47:58 +0100

    task finished

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create A data Base
+-- hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Script showing all table of a DB
+-- the selected DB here is mysql
+USE mysql; SHOW TABLES;
