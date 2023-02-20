3ba8c1d8f3b7ada2894214c4522bb3b1a5b7a0e6
ibukun <iadewaleodusanya@gmail.com>
2023-02-20 04:41:47 +0100

    pudh

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- a script that lists all rows of the table
+-- Query the DB for a list of rows
+SELECT * FROM first_table
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- A script that displays the number of records with id = 89 in the table
+-- Query DB for record
+SELECT COUNT(*) FROM first_table WHERE id = 89
