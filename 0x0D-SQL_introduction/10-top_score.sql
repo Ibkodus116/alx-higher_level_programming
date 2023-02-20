896a95d73901173c8037093b2cb832eba6c29e5d
ibukun <iadewaleodusanya@gmail.com>
2023-02-20 05:27:44 +0100

    10-12

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- A script that lists all records of the table
+-- Query for table data
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- A script that updates the score of Bob to 10 in the table
+-- Query the DB for an update
+UPDATE second_table SET score = 10 WHERE name = "Bob"
