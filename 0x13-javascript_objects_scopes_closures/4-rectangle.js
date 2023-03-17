1cc2e0b0406f4d5094443271dbfcdb801848ac6f
ibukun <iadewaleodusanya@gmail.com>
2023-03-17 15:55:00 +0100

    completetd

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let s = '';
+    for (let i = 0; i < this.width; i++) {
+      s += 'X';
+    }
+    for (let x = 0; x < this.height; x++) {
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
