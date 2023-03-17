4f50e037f804b4130509cac664d620b60b839137
ibukun <iadewaleodusanya@gmail.com>
2023-03-17 18:14:46 +0100

    code refactored

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-function esrever (list) {
+exports.esrever = function (list) {
   const nList = [];
-  const size = list.length - 1;
-  for (let i = size; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     nList.push(list[i]);
   }
   return nList;
-}
-exports.esrever = function (list) {
-  return esrever(list);
 };
