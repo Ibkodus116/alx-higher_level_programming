d7e27561f30723d3954bfa6c697ac6b664164539
ibukun <iadewaleodusanya@gmail.com>
2023-03-17 15:03:14 +0100

    semi***

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {   
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
     let s = '';
     for (let i = 0; i < this.width; i++) {
