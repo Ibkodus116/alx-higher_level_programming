bef1d145472f51bceacbf87aaee8ee848a36b117
ibukun <iadewaleodusanya@gmail.com>
2023-03-21 07:53:43 +0100

    [completed] Task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates a new MySQL server user and grant permission
+-- Query CREATE and GRANT
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
