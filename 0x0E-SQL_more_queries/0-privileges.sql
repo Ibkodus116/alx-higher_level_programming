83e3546decbef437f74f982252b4596cc4761357
ibukun <iadewaleodusanya@gmail.com>
2023-03-21 07:19:34 +0100

    initial commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that list all priviledge of some MySQL users
+-- using the GRANT Query in MySQL
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+this is the MySQL README file ☺️
