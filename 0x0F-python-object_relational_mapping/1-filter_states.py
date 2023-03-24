1bab43ecaef43b362a4d75195a59af2822d01346
ibukun <iadewaleodusanya@gmail.com>
2023-03-24 18:42:52 +0100

    fixed Upper case restriction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     cur.execute(
         """
         SELECT id, name FROM states WHERE
-        name LIKE 'N%' ORDER BY states.id ASC
+        BINARY name LIKE 'N%' ORDER BY states.id ASC
         """
         )
 
