8990db81289023c8d27f5d45d7e2888b6bccb64c
ibukun <iadewaleodusanya@gmail.com>
2023-03-24 14:13:15 +0100

    Fixed ERROR

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
+"""
+This is a Script that query My SQL
+"""
 import MySQLdb
 import sys
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+    # connect to MySQL server
+    db = MySQLdb.connect(
+            host='localhost', user=mysql_username,
+            passwd=mysql_password, db=database_name
+            )
 
+    # Create a cursor object
+    cursor = db.cursor()
 
-print(sys.argv[1], sys.argv[2], sys.argv[3])
+    # Execute SQL query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# connect to MySQL server
-db = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, db=database_name)
+    # Fecth all rows
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-# Create a cursor object
-cursor = db.cursor()
-
-# Execute SQL query
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-# Fecth all rows
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
