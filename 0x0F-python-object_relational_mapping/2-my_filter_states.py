9a924349a42e5300bacbb3cc59df2ec1389332a9
ibukun <iadewaleodusanya@gmail.com>
2023-03-25 05:21:06 +0100

    fixed import isssue

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,29 +8,30 @@ name matches the argument.
 import MySQLdb
 from sys import argv
 
-mysql_username = argv[1]
-mysql_password = argv[2]
-database_name = argv[3]
-state_name = argv[4]
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
 
-# Connect to SQL
-db = MySQLdb.connect(
-    db=database_name,
-    host='localhost',
-    user=mysql_username,
-    passwd=mysql_password,
-    )
+    # Connect to SQL
+    db = MySQLdb.connect(
+        db=database_name,
+        host='localhost',
+        user=mysql_username,
+        passwd=mysql_password,
+        )
 
-# Create a cursor for your connection
-cur = db.cursor()
+    # Create a cursor for your connection
+    cur = db.cursor()
 
-# Excecute MySQL Queries
+    # Excecute MySQL Queries
 
-cur.execute(
-    'SELECT * FROM states WHERE name=%s ORDER BY id ASC',
-    (state_name,)
-    )
+    cur.execute(
+        'SELECT * FROM states WHERE name=%s ORDER BY id ASC',
+        (state_name,)
+        )
 
-
-for row in cur.fetchall():
-    print(row)
+# Printing each row
+    for row in cur.fetchall():
+        print(row)
