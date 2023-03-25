d707e7460cd5fdb8d455d2bab6a312041a9cd2fd
ibukun <iadewaleodusanya@gmail.com>
2023-03-25 06:38:50 +0100

    ASC included

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # execute the cursor
     cur.execute("""SELECT cities.id, cities.name, states.name
                 FROM cities, states
-                WHERE cities.state_id = states.id ORDER BY cities.id;
+                WHERE cities.state_id = states.id ORDER BY cities.id ASC;
                 """
                 )
 
