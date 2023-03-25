8712488a99cf6f5515fb75e6333b9b95ea7d3af2
ibukun <iadewaleodusanya@gmail.com>
2023-03-25 14:16:31 +0100

    fixed document

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+A script that lists all State
+objects from the database hbtn_0e_6_usa
+"""
+
 from sys import argv
 from model_state import State, Base
 from sqlalchemy import create_engine
