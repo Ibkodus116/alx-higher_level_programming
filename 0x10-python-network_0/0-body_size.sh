2cd2e4c3cab00727e004a3c4298b30ebbbd9059a
ibukun <iadewaleodusanya@gmail.com>
2023-04-28 13:13:09 +0100

    complted

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if the script was called with a URL
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the URL and store the response body in a variable
+# response=$(curl -sI $1 | grep -i content-length | awk '{print $2}' | tr -d '\r')
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+# Display the size of the response body
+#echo "this is $response"
