194a48f7fc63a33911f1760f3b32486bd2af29ec
ibukun <iadewaleodusanya@gmail.com>
2023-05-02 06:56:19 +0100

    fixed typos

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ with urllib.request.urlopen(url) as response:
     html = response.read()
 
 print('Body response:')
-print(f'\t - type: {type(html)}')
-print(f'\t - content: {html}')
-print(f'\t - utf8 content: {html.decode("utf-8")}')
+print(f'\t- type: {type(html)}')
+print(f'\t- content: {html}')
+print(f'\t- utf8 content: {html.decode("utf-8")}')
