fe6aac09b8b2d2fe1da5db594a25fbe40c927d2a
ibukun <iadewaleodusanya@gmail.com>
2023-05-02 11:52:44 +0100

    complted

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,5 +11,5 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    data = requests.get('X-Request-Id')
-    print(data)
+    data = requests.get(url)
+    print(data.header.get('X-Request-Id'))
